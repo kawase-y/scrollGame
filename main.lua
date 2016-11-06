f043d71ce05e38bf9bf30f9dc93ac3058f64b6ba
kawase-y <lovol0919@gmail.com>
2016-11-07 01:22:16 +0900

    ブロック崩し化

--- main.lua
+++ main.lua
@@ -1,10 +1,4 @@
------------------------------------------------------------------------------------------
---
--- main.lua
---uoooooooooooooooooooooooooooooo
------------------------------------------------------------------------------------------
 
--- Your code here
 
 --ライブラリの指定
 local scroll  = require("scroll")
@@ -14,57 +8,127 @@ local widget  = require "widget"
 --定数
 local _W = display.contentWidth
 local _H = display.contentHeight
-local resetBtn
 
+-- 物理演算開始
+physics.start()
+physics.setGravity(0, 0)
 
 --背景設定
 local Back
 bg 					= display.newRect( 0, 0, _W, _H )
 bg.anchorX 	= 0
 bg.anchorY 	= 0
-bg:setFillColor( 0,0,0 )
+bg:setFillColor( 0,0.5,0.5 )
 
--- 物理演算開始
-physics.start()
-
--- 壁の定義
+------------------------------------------------------------------------------
+--壁の定義
+------------------------------------------------------------------------------
 local walls = {
             display.newRect(3, _H/2, 6, _H), -- 左の壁
             display.newRect(_W/2, 3, _W, 6), -- 上の壁
             display.newRect(_W -3, _H/2, 6, _H), -- 右の壁
-            display.newRect(_W/2, _H -3, _W, 6) -- 下の壁
       }
-          for i=1, 4, 1 do -- 壁の初期設定
-              walls[i]:setFillColor(1, 1, 1) -- 白
-              physics.addBody( walls[i], "static", {} ) -- 壁は重力で動かない静的なオブジェ
-          end
+for i=1, #walls, 1 do -- 壁の初期設定
+        walls[i]:setFillColor(1, 1, 1) -- 白
+        physics.addBody( walls[i], "static", {density = 0.0, friction = 0.0, bounce = 1.0} ) -- 壁は重力で動かない静的なオブジェ
+        walls[i].tag = "wall"
+end
 
---オブジェクトの定義
-local myBall    = display.newImage("ball.png",50,50) -- プレイヤー
-local SE_coin   = audio.loadSound("Coin.mp3") -- コインの効果音
-local coin      = display.newImage("coin.png", math.random(40, _W-40), math.random(40, _H-40)) -- コイン
-
-Gallery = widget.newButton{
-  label        = "Reset",
-  labelColor   = { default={255}, over={128} },
-  defaultFile  = "btn.png",
-  overFile     = "btnover.png",
-  width=70, height=35,
-  emboss = true,
-  onRelease = onresetBtnRelease	-- event listener function
-}
-Gallery.x = 70
-Gallery.y = _H + 25
-
-physics.addBody(myBall, {density = 2, friction = 0.2, bounce = 1.005, radius = 20}) --ボールの物理属性
-physics.addBody(coin, "kinematic", {isSensor = true}) --コインの物理属性
+local bottomWall = display.newRect(_W/2, _H -3, _W, 6) -- 下の壁
+bottomWall:setFillColor(1, 1, 1) -- 白
+physics.addBody( bottomWall, "static", {density = 0.0, friction = 0.0, bounce = 1.0} ) -- 壁は重力で動かない静的なオブジェ
+bottomWall.tag = "bottomWall"
 
 
-display.setStatusBar(display.HiddenStatusBar) -- 不明
+------------------------------------------------------------------------------
+ -- ボールの定義、ゲーム開始設定
+------------------------------------------------------------------------------
 
+local myBall = display.newImage("ball.png", 100, 100) -- ボール
+physics.addBody(myBall, {density = 0.0, friction = 0.0, bounce = 1.0}) --ボールの物理属性
+myBall.tag = "ball"
+
+
+function resetBallPos()
+    myBall.x = _W/2
+    myBall.y = _H/2
+end
+
+function gameStart()
+    resetBallPos()
+    myBall:setLinearVelocity(0, 100) -- y方向の初速度
+end
+
+gameStart()
+
+------------------------------------------------------------------------------
+ -- ブロックの定義
+------------------------------------------------------------------------------
+
+local maxNumBlocks = 0 -- ブロックの最大数
+local numBlocks = 0
+local blocks = {}
+
+function deleteBlock(index)
+    -- ブロックが存在しない場合は無視する
+    if (blocks[index] == nil) then
+        return -- ここで関数を終了させる
+    end
+      blocks[index]:removeSelf() -- 画面から消す関数
+      blocks[index] = nil -- メモリ解放
+      numBlocks = numBlocks - 1 -- 一つブロックを削除したので、 numBlocksを -1する
+end
+
+function deleteAllBlocks()
+    -- for文でブロックを全て削除
+    for i = 0, maxNumBlocks, 1 do
+        deleteBlock(i)
+    end
+      -- ブロックを管理している変数を全て初期化する
+      maxNumBlocks = 0
+      numBlocks = 0
+      blocks = {}
+end
+
+function deployBlocks()
+    -- ブロックを配置する前に全てのブロックを削除
+    deleteAllBlocks()
+
+    -- ブロックを配置
+    for y = 0, 1, 1 do
+        for x = 0, 4, 1 do
+            -- 何番目の要素か
+            local index = x + (y * 5) -- indexは0-9まで
+            blocks[index] = display.newImage("block.png", _W * 1/8, 100) -- この後再配置するためこの座標(_W * 1/8, 100)は意味なし
+
+            -- (width * 1/6) => 画面を6つに分ける、2つは両端なので、実際に使えるのは4つ
+            -- (x + 1) => 分けた4つのうちの何番目か、0は端っこなので+1して無視する
+            blocks[index].x = (x + 1) * _W/6
+
+            blocks[index].y = _H/6 + (_H/6 * y)
+            blocks[index].tag = "block"
+
+            blocks[index].index = index  -- 後で識別しやすいように生成した順番を入れておく
+            physics.addBody(blocks[index], "static", {density = 0.0, friction = 0.0, bounce = 1.0})
+
+            -- 現在のブロック数を追加
+            numBlocks = numBlocks + 1
+        end
+    end
+
+    -- 生成したブロック数を保存
+    maxNumBlocks = numBlocks
+end
+
+deployBlocks()
+
+------------------------------------------------------------------------------
+ -- 線の定義
+------------------------------------------------------------------------------
 
 local myLines    = {} -- 線の定義
 local lineCount  = 1 -- 線の初期値
+local maxLineNum = 100
 local resetJudge = false -- 線の数の最大値を判定
 local tx; local ty; -- 線の始点
 
@@ -72,7 +136,7 @@ local tx; local ty; -- 線の始点
 
 function DrawLine(event) -- 線を書く(最重要)
 
-    if(event.y >= _H/20)then -- あまり高い位置では線を書かせない
+    if(event.y >= _H/3*2)then -- 下1/3の範囲でのみ描画可能
         local t     = event.target
         local phase = event.phase
 
@@ -85,9 +149,10 @@ function DrawLine(event) -- 線を書く(最重要)
               ty                              = event.y;
               myLines[lineCount]              = display.newLine(tx, ty, tx+5, ty)
               myLines[lineCount].strokeWidth  = 5
-              physics.addBody(myLines[lineCount], "kinematic",{})
+              physics.addBody(myLines[lineCount], "static",{density = 0.0, friction = 0.0, bounce = 1.0})
+              myLines[lineCount].tag = "var"
 
-                      if (lineCount == 100) then -- 線(点のつながり)の最大は100個
+                      if (lineCount == maxLineNum) then -- 線(点のつながり)の最大は100個
                           lineCount   = 1
                           resetJudge  = true
                           display.remove(myLines[1])
@@ -103,37 +168,80 @@ function DrawLine(event) -- 線を書く(最重要)
     end
 end
 
-local function onresetBtnRelease() -- 線のリセット
-  for i=1, 100 do
-    display.remove(myLines[i])
-  end
+------------------------------------------------------------------------------
+ -- ゲームロジック
+------------------------------------------------------------------------------
+
+function ballStabilization()
+    -- 速度を取得して、x,yの速度を500に固定する
+    local vx, vy = myBall:getLinearVelocity()
+      if (0 < vx) then
+          vx = 300
+      else
+          vx = -300
+      end
+      if (0 < vy) then
+          vy = 300
+      else
+          vy = -300
+      end
+    -- 速度を安定させる
+    myBall:setLinearVelocity(vx, vy)
+    -- 回転させる
+    --myBall:applyTorque(90)
 end
 
-local function onCollision(event) -- コインとの当たり判定
 
-    if(event.phase == "began")then
-        if(coin.isVisible) then
-      audio.play(SE_coin)
-      coin.isVisible = false
+local completeText = nil
+
+function completeGame()
+    physics.pause()
+    completeText = display.newText("Complete", _W/2, _H/2, native.systemFont, 40)
+    completeText:setTextColor(1.0, 1.0, 1.0)
+     Runtime:addEventListener("tap", resetGame)
+end
+
+function failGame()
+    physics.pause()
+    completeText = display.newText("Fail", _W/2, _H/2, native.systemFont, 40)
+    completeText:setTextColor(1.0, 1.0, 1.0)
+     Runtime:addEventListener("tap", resetGame)
+end
+
+function resetGame()
+    Runtime:removeEventListener("tap", resetGame)
+
+    completeText:removeSelf()
+    completeText = nil
+
+    physics.start()
+
+    deployBlocks()
+    resetBallPos()
+    gameStart()
+end
+
+function ballCollision(event)
+    if (event.phase == "began") then
+        print("collision: "..event.other.tag)
+    elseif (event.phase == "ended") then
+        ballStabilization()
+
+        -- ブロックに当たった時はブロックを削除
+        if (event.other.tag == "block") then
+            local hitBlock = event.other
+            deleteBlock(hitBlock.index)
+            -- ブロックがなくなった場合はクリア判定
+            if (numBlocks == 0) then
+              completeGame()
+            end
+        elseif (event.other.tag == "bottomWall") then
+              failGame()
         end
     end
 end
 
-local function coinReset(event) -- コインの再配置,ゲームオーバーの判定
-  -- コインの再配置
-  if(coin.isVisible == false) then
-     coin.x = math.random(40, _W-40)
-     coin.y = math.random(40, _H-40)
-     coin.isVisible = true
-  end
-
-  -- ゲームオーバーの表示
-    if(myBall.y > _H)then
-        Gameover = display.newText("GAMEOVER",100,400,nil,30)
-        Gameover:setFillColor(1,1,1)
-    end
-end
-
-Runtime:addEventListener("touch", DrawLine)       -- 線を引く(最重要)
-Runtime:addEventListener("enterFrame",coinReset)  -- コインの再配置,ゲームオーバーの判定
-coin:addEventListener("collision", onCollision)   -- コインとの当たり判定
+-- 衝突イベントをボールに設定
+myBall:addEventListener("collision", ballCollision)
+-- 線を引く(最重要)
+Runtime:addEventListener("touch", DrawLine)
